7a59f1fe83ec92f889bd2499d76b64bf215cb203
Denis Falkowski <falkowski.denis@gmail.com>
2020-10-15 15:35:47 +0300

    feat: add solution for Reverse-Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    let arr = [];
+    let str = '';
+    arr = String(n).split('').reverse();
+    if (n > 0) {
+    for (let i = 0; i < arr.length; i++) {
+        str += arr[i];
+    }
+  return +str;
+} else {
+    for (let i = 0; i < arr.length - 1; i++) {
+        str += arr[i];
+    }
+  return +str;
 }
+};
